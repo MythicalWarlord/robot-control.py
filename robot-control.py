96a09346f76fa5837e1f1170eb4c58f834561736
hax0r <zthampton01@gmail.com>
2022-10-27 16:29:48 -0400

    Intermediate script for testing

--- robot-control.py
+++ robot-control.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
 from pyPS4Controller.controller import Controller
-import bluetool
 
+
+# defining class for controller
+class MyController(Controller):
+    # initialize attributes for controller
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+
+    def on_x_press(self):
+        print("Hello world")
+
+    def on_x_release(self):
+        print("Goodbye world")
+
+
+# pointing to where controller is connected to and listening for inputs
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen()
